57aa950583c4d0bbde6fc5cd6e4af24693ec5f29
Jhojan98 <jhojanramirez75@gmail.com>
2023-11-19 18:30:28 -0500

    Update equipo.py

---
+++ dao/equipo.py
@@ -0,0 +1,54 @@
+rom db.connection import with_connection
+
+class EquipoDAO():
+    def data(self,
+            id,
+            nombre,
+            lider,
+            maraton,
+            estado):
+        self._id = id
+        self._nombre = nombre
+        self._lider = lider
+        self._maraton = maraton
+        self._estado = estado
+
+    @with_connection
+    def insert_equipo(self, *args, **kwargs):
+        conn = kwargs.pop('connection')
+        cursor = conn.cursor()
+        query = f'''
+            INSERT INTO equipo
+            (id,
+            nombre,
+            lider,
+            maraton,
+            estado)
+            VALUES(?,?,?,?,?)
+        '''
+        cursor.execute(query,
+                                (self._id,
+                                self._nombre,
+                                self._lider,
+                                self._maraton,
+                                self._estado)
+        )
+        return cursor.lastrowid
+    
+
+    @with_connection
+    def select_equipo(self, nombre, *args, **kwargs):
+        conn = kwargs.pop('connection')
+        cursor = conn.cursor()
+        query = f'''
+            SELECT id,
+                nombre,
+                lider,
+                maraton,
+                estado
+            FROM equipo
+            WHERE nombre = ?
+        '''
+        cursor.execute(query, (nombre,))
+        result = cursor.fetchone()
+        return result
